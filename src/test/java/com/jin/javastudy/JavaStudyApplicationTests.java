89250ece57cf9ffd19e2433a847234a518b0547b
hjlee <hjlee@k2systems.kr>
2025-02-20 09:31:54 +0900

    Initial commit

---
+++ src/test/java/com/jin/javastudy/JavaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jin.javastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
